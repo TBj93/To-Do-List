8aff0355eefe7d4d07dca65ab9bf72595c9c5b0b
TBj93 <Tim-Burmester1@web.de>
2022-05-19 00:44:16 +0200

    Added  dbclick function and list from input

--- scripts.js
+++ scripts.js
@@ -1,10 +1,39 @@
-function showMsg(item) {
-    alert(item);
-}
 
 function newItem() {
- 
-var str =$('#input').val();
-alert(str);
+    let str =$('#input').val();
+let list = $('#list');
+let li = $('<li></li>');
+list.append(li);
+li.append(str);
+
+$('li').dblclick(function() {
+    li.addClass("strike");
+    });
 
 }
+
+
+
+
+/*
+
+$('#list').append(<li>dsgsdfg</li>);
+'+str+'\r\n'+'
+
+let str =$('#input').val();
+
+li.on("click", function() {
+    li.addClass("strike")
+});
+
+/*
+
+$('li').dblclick(function() {
+li.addClass("strike");
+});
+
+/*
+$("#list" ).dblclick(function() {
+    alert( "Handler for .dblclick() called." );
+  });
+*/
